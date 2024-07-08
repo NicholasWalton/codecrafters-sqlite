209704080502448b29ab3e84d2877b6e54291257
Nicholas Walton <nicholas.walton@gmail.com>
2024-07-08 10:03:46 -0700

    dbinfo: refactor

--- app/main.py
+++ app/main.py
@@ -9,23 +9,20 @@ from dataclasses import dataclass
 class DbInfo():
     page_size: int = 0
 
+    def __init__(self, database_file_path):
+        with open(database_file_path, "rb") as database_file:
+            database_file.seek(16)  # Skip the first 16 bytes of the header
+            self.page_size = int.from_bytes(database_file.read(2), byteorder="big")
+
 def main():
     database_file_path = sys.argv[1]
     command = sys.argv[2]
 
     if command == ".dbinfo":
-        page_size = db_info(database_file_path)
+        page_size = DbInfo(database_file_path).page_size
         print(f"database page size: {page_size}")
     else:
         print(f"Invalid command: {command}")
-
-
-def db_info(database_file_path):
-    db_info = DbInfo()
-    with open(database_file_path, "rb") as database_file:
-        database_file.seek(16)  # Skip the first 16 bytes of the header
-        db_info.page_size = int.from_bytes(database_file.read(2), byteorder="big")
-    return db_info
 
 
 if __name__ == '__main__':
--- test_dbinfo.py
+++ test_dbinfo.py
@@ -1,4 +1,4 @@
 import app.main
 
 def test_page_size():
-    assert app.main.db_info("sample.db").page_size == 4096
+    assert app.main.DbInfo("sample.db").page_size == 4096
