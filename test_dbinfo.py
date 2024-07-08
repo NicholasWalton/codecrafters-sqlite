40fc070c474d9bad2f9335328808cccd50520f4b
Nicholas Walton <nicholas.walton@gmail.com>
2024-07-08 10:27:54 -0700

    dbinfo: Scaffolding for many-tables test

--- test_dbinfo.py
+++ test_dbinfo.py
@@ -1,3 +1,5 @@
+import pytest
+
 import app.main
 
 import sqlite3
@@ -12,3 +14,13 @@ def test_number_of_tables():
         assert con.execute("SELECT count(*) FROM sqlite_schema").fetchall() == [(3,)]
 
     assert app.main.DbInfo("sample.db").number_of_tables == 3
+
+
+def test_far_too_many_tables(tmp_path):
+    expected_tables = 3
+    with sqlite3.connect(tmp_path / "test.db") as db:
+        for i in range(expected_tables):
+            db.execute("CREATE TABLE dummy%d (value int);" % i)
+        db.commit()
+        assert db.execute("SELECT count(*) FROM sqlite_schema").fetchall() == [(expected_tables,)]
+    assert app.main.DbInfo("sample.db").number_of_tables == expected_tables
