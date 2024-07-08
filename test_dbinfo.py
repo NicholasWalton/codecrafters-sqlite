29528b169851a7e6d92db16bad8bf33167b115fa
Nicholas Walton <nicholas.walton@gmail.com>
2024-07-08 10:10:17 -0700

    dbinfo: check table count with sqlite3 library

--- test_dbinfo.py
+++ test_dbinfo.py
@@ -1,4 +1,13 @@
 import app.main
 
+import sqlite3
+
 def test_page_size():
     assert app.main.DbInfo("sample.db").page_size == 4096
+
+
+def test_number_of_tables():
+    with sqlite3.connect("sample.db") as con:
+        assert con.execute("SELECT count(*) FROM sqlite_schema").fetchall() == [(3,)]
+
+    # assert app.main.DbInfo("sample.db").number_of_tables == 3
