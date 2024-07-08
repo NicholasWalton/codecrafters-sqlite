84dc0bd71ae12bcb397973967270e521deab1033
Nicholas Walton <nicholas.walton@gmail.com>
2024-07-08 09:47:21 -0700

    dbinfo: Refactor for testability

--- app/main.py
+++ app/main.py
@@ -4,17 +4,24 @@ from dataclasses import dataclass
 
 # import sqlparse - available if you need it!
 
-database_file_path = sys.argv[1]
-command = sys.argv[2]
 
-if command == ".dbinfo":
+def main():
+    database_file_path = sys.argv[1]
+    command = sys.argv[2]
+
+    if command == ".dbinfo":
+        page_size = db_info(database_file_path)
+        print(f"database page size: {page_size}")
+    else:
+        print(f"Invalid command: {command}")
+
+
+def db_info(database_file_path):
     with open(database_file_path, "rb") as database_file:
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
-
-        # Uncomment this to pass the first stage
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
-        print(f"database page size: {page_size}")
-else:
-    print(f"Invalid command: {command}")
+        return page_size
+
+
+if __name__ == '__main__':
+    main()
