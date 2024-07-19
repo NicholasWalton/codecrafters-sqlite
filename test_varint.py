e687ddbca906d45c9c63cbdb19473529a000913b
Nicholas Walton <nicholas.walton@gmail.com>
2024-07-19 08:49:09 -0700

    Refactor

--- test_varint.py
+++ test_varint.py
@@ -1,8 +1,14 @@
 
 def huffman_decode(varint):
-    if varint[0] & 0b1000_0000:
-        return ((varint[0] & 0b0111_1111) << 8) + varint[1]
+    if _high_bit(varint[0]):
+        return (_lower7(varint[0]) << 8) + varint[1]
     return varint[0]
+
+def _high_bit(byte):
+    return byte & 0b1000_0000
+
+def _lower7(byte):
+    return (byte & 0b0111_1111)
 
 def test_zero_value_15_bit_huffman():
     assert huffman_decode(bytearray((0b0000_0000,))) == 0
