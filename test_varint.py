94281622ab9a204320e7604147d69504a7e9f2d8
Nicholas Walton <nicholas.walton@gmail.com>
2024-07-19 09:08:03 -0700

    refactor: Parametrize 15-bit Huffman tests

--- test_varint.py
+++ test_varint.py
@@ -1,3 +1,4 @@
+import pytest
 
 def huffman_decode(varint):
     if _high_bit(varint[0]):
@@ -14,23 +15,14 @@ def _high_bit(byte):
 def _lower7(byte):
     return (byte & 0b0111_1111)
 
-def test_zero_value_15_bit_huffman():
-    assert huffman_decode(bytearray((0b0000_0000,))) == 0
-
-def test_one_value_15_bit_huffman():
-    assert huffman_decode(bytearray((0b0000_0001,))) == 1
-
-def test_two_byte_15_bit_huffman():
-    assert huffman_decode(bytearray((0b1000_0000, 0b1000_0001))) == 129
-
-def test_256_value_15_bit_huffman():
-    assert huffman_decode(bytearray((0b1000_0001, 0b0000_0000))) == 256
-
-def test_max_15_bit_huffman():
-    assert huffman_decode(bytearray((0b1011_1111, 0b1111_1111))) == 16383
-
-def test_negative_15_bit_huffman():
-    assert huffman_decode(bytearray((0b1111_1111, 0b1111_1111))) == -1
-
-def test_min_15_bit_huffman():
-    assert huffman_decode(bytearray((0b1100_0000, 0b0000_0000))) == -16384
+@pytest.mark.parametrize("byte_tuple,expected", (
+        ((0b0000_0000,), 0),
+        ((0b0000_0001,), 1),
+        ((0b1000_0000, 0b1000_0001), 129),
+        ((0b1000_0001, 0b0000_0000), 256),
+        ((0b1011_1111, 0b1111_1111), 16383),
+        ((0b1111_1111, 0b1111_1111), -1),
+        ((0b1100_0000, 0b0000_0000), -16384),
+))
+def test_15_bit_huffman(byte_tuple, expected):
+    assert huffman_decode(bytearray(byte_tuple)) == expected
