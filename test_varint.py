625973a3955f83c2d8191d2d23b0c3a56c4208a0
Nicholas Walton <nicholas.walton@gmail.com>
2024-07-19 09:00:48 -0700

    Finish 15 bit huffman

--- test_varint.py
+++ test_varint.py
@@ -1,7 +1,11 @@
 
 def huffman_decode(varint):
     if _high_bit(varint[0]):
-        return (_lower7(varint[0]) << 8) + varint[1]
+        unsigned = (_lower7(varint[0]) << 8) + varint[1]
+        if unsigned & (1 << 14):
+            return unsigned - (1 << 15)
+            # raise Exception("Negative!")
+        return unsigned
     return varint[0]
 
 def _high_bit(byte):
@@ -21,3 +25,12 @@ def test_two_byte_15_bit_huffman():
 
 def test_256_value_15_bit_huffman():
     assert huffman_decode(bytearray((0b1000_0001, 0b0000_0000))) == 256
+
+def test_max_15_bit_huffman():
+    assert huffman_decode(bytearray((0b1011_1111, 0b1111_1111))) == 16383
+
+def test_negative_15_bit_huffman():
+    assert huffman_decode(bytearray((0b1111_1111, 0b1111_1111))) == -1
+
+def test_min_15_bit_huffman():
+    assert huffman_decode(bytearray((0b1100_0000, 0b0000_0000))) == -16384
