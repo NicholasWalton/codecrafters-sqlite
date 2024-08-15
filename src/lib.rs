2da6ff236cb9a02dfb044d759b69f806d4b3d422
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-15 09:42:15 -0700

    rust: reformat and refactor

--- src/lib.rs
+++ src/lib.rs
@@ -6,34 +6,37 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+const HUFFMAN_LENGTH: usize = 9;
+
 #[pyfunction]
 fn varint(buffer: Vec<u8>) -> PyResult<(i64, usize)> {
+    let mut acc = 0i64;
 
-    let huffman_length = 9;
-    let mut unsigned = 0i64; // TODO: needs to be signed?
-
-    let mut byte_index = applesauce(&buffer, huffman_length, &mut unsigned);
-    if byte_index == huffman_length - 2 {
-        byte_index += 1;
-        unsigned = (unsigned << 8) + buffer[byte_index] as i64;  // or all 8 bits of the nth byte
+    let mut byte_index = decode_leading_bytes(&buffer, &mut acc);
+    if byte_index == HUFFMAN_LENGTH - 1 {
+        // or all 8 bits of the nth byte
+        acc = (acc << 8) + buffer[byte_index] as i64;
     }
-    let huffman_bits = 7 * huffman_length + 1;
+    let huffman_bits = 7 * HUFFMAN_LENGTH + 1;
     let sign_bit = 1 << (huffman_bits - 1);
-    if (unsigned & sign_bit) != 0 {
-        Ok((unsigned - (sign_bit << 1), byte_index + 1))
+    if (acc & sign_bit) != 0 {
+        Ok((acc - (sign_bit << 1), byte_index + 1))
     } else {
-        Ok((unsigned, byte_index + 1))
+        Ok((acc, byte_index + 1))
     }
 }
 
-fn applesauce(buffer: &Vec<u8>, huffman_length: usize, unsigned: &mut i64) -> usize {
-    for byte_index in 0..huffman_length - 1 { // zero or more bytes which have the high-order bit set
-        *unsigned = (*unsigned << 7) + _lower7(buffer.get(byte_index).unwrap()) as i64; // The lower seven bits of each of the first n-1 byte
-        if 0 == _high_bit(buffer.get(byte_index).unwrap()) {  // including a single end byte with the high-order bit clear
+fn decode_leading_bytes(buffer: &Vec<u8>, acc: &mut i64) -> usize {
+    // zero or more bytes which have the high-order bit set
+    for byte_index in 0..HUFFMAN_LENGTH - 1 {
+        // The lower seven bits of each of the first n-1 byte
+        *acc = (*acc << 7) + _lower7(buffer.get(byte_index).unwrap()) as i64;
+        // including a single end byte with the high-order bit clear
+        if 0 == _high_bit(buffer.get(byte_index).unwrap()) {
             return byte_index;
         }
     }
-    huffman_length - 2
+    HUFFMAN_LENGTH - 1
 }
 
 fn _high_bit(byte: &u8) -> u8 {
@@ -74,17 +77,19 @@ mod test {
     #[test]
     fn nine_bytes() {
         assert_eq!(
-            varint(
-                vec![0b1000_0001,
-                    0b1000_0000,
-                    0b1000_0000,
-                    0b1000_0000,
-                    0b1000_0000,
-                    0b1000_0000,
-                    0b1000_0000,
-                    0b1000_0000,
-                    0b0000_0000,]
-            ).unwrap().0,
+            varint(vec![
+                0b1000_0001,
+                0b1000_0000,
+                0b1000_0000,
+                0b1000_0000,
+                0b1000_0000,
+                0b1000_0000,
+                0b1000_0000,
+                0b1000_0000,
+                0b0000_0000,
+            ])
+            .unwrap()
+            .0,
             1 << 57
         );
     }
