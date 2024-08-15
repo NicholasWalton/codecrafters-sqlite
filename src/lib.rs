38df4b6766a6158cfd743246e85d14154a7ec0f5
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-15 07:54:10 -0700

    pyo3: Start implementing varint() in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pyfunction]
-fn varint(buffer: &[u8]) -> PyResult<u64> {
+fn varint(buffer: Vec<u8>) -> PyResult<u64> {
     println!("Hello {buffer:?}");
-    return Ok(0)
+    return Ok(buffer[0] as u64)
 }
 
 /// A Python module implemented in Rust.
@@ -18,4 +18,19 @@ fn codecrafters_sqlite(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(varint, m)?)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::varint;
+
+    #[test]
+    fn zero() {
+        assert_eq!(varint(vec![0]).unwrap(), 0u64);
+    }
+
+    #[test]
+    fn one() {
+        assert_eq!(varint(vec![1]).unwrap(), 1u64);
+    }
 }
