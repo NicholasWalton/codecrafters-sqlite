d7c5be63943861e245fd08eb0b20d5fed2a85fa0
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-15 09:03:32 -0700

    rust varint: remove dbg!()s

--- src/lib.rs
+++ src/lib.rs
@@ -8,27 +8,27 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 #[pyfunction]
 fn varint(buffer: Vec<u8>) -> PyResult<(i64, usize)> {
-    dbg!(&buffer);
+
     let huffman_length = 9;
     let mut unsigned = 0i64; // TODO: needs to be signed?
 
     let mut byte_index = applesauce(&buffer, huffman_length, &mut unsigned);
     if byte_index == huffman_length - 2 {
         byte_index += 1;
-        dbg!(unsigned = (unsigned << 8) + buffer[byte_index] as i64);  // or all 8 bits of the nth byte
+        unsigned = (unsigned << 8) + buffer[byte_index] as i64;  // or all 8 bits of the nth byte
     }
     let huffman_bits = 7 * huffman_length + 1;
     let sign_bit = 1 << (huffman_bits - 1);
     if (unsigned & sign_bit) != 0 {
-        Ok((dbg!(unsigned - (sign_bit << 1)), byte_index + 1))
+        Ok((unsigned - (sign_bit << 1), byte_index + 1))
     } else {
-        Ok((dbg!(unsigned), byte_index + 1))
+        Ok((unsigned, byte_index + 1))
     }
 }
 
 fn applesauce(buffer: &Vec<u8>, huffman_length: usize, unsigned: &mut i64) -> usize {
     for byte_index in 0..huffman_length - 1 { // zero or more bytes which have the high-order bit set
-        *unsigned = dbg!((*unsigned << 7) + _lower7(buffer.get(byte_index).unwrap()) as i64); // The lower seven bits of each of the first n-1 byte
+        *unsigned = (*unsigned << 7) + _lower7(buffer.get(byte_index).unwrap()) as i64; // The lower seven bits of each of the first n-1 byte
         if 0 == _high_bit(buffer.get(byte_index).unwrap()) {  // including a single end byte with the high-order bit clear
             return byte_index;
         }
