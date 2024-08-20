9a13a0c9bdac2862a3e8a70187f073c5b8cf77fb
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-20 08:04:04 -0700

    rust: fix warning

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ const HUFFMAN_LENGTH: usize = 9;
 fn varint(buffer: Vec<u8>) -> PyResult<(i64, usize)> {
     let mut acc = 0i64;
 
-    let mut byte_index = decode_leading_bytes(&buffer, &mut acc);
+    let byte_index = decode_leading_bytes(&buffer, &mut acc);
     if byte_index == HUFFMAN_LENGTH - 1 {
         // or all 8 bits of the nth byte
         acc = (acc << 8) + buffer[byte_index] as i64;
@@ -24,20 +24,20 @@ fn decode_leading_bytes(buffer: &Vec<u8>, acc: &mut i64) -> usize {
     // zero or more bytes which have the high-order bit set
     for byte_index in 0..HUFFMAN_LENGTH - 1 {
         // The lower seven bits of each of the first n-1 byte
-        *acc = (*acc << 7) + _lower7(buffer.get(byte_index).unwrap()) as i64;
+        *acc = (*acc << 7) + i64::from(_lower7(*buffer.get(byte_index).unwrap()));
         // including a single end byte with the high-order bit clear
-        if 0 == _high_bit(buffer.get(byte_index).unwrap()) {
+        if 0 == _high_bit(*buffer.get(byte_index).unwrap()) {
             return byte_index;
         }
     }
     HUFFMAN_LENGTH - 1
 }
 
-fn _high_bit(byte: &u8) -> u8 {
+fn _high_bit(byte: u8) -> u8 {
     byte & 0b1000_0000
 }
 
-fn _lower7(byte: &u8) -> u8 {
+fn _lower7(byte: u8) -> u8 {
     byte & 0b0111_1111
 }
 
